0b9f1a827051b38030a2380d31e33f6d875f0557
Jacob Deepu <d2jdeepu@gmail.com>
2021-05-12 21:51:28 +0530

    Add window rules

--- config.h
+++ config.h
@@ -27,9 +27,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class              instance    title       tags mask     isfloating   monitor */
+	{ "Blueman-manager",  NULL,       NULL,       1 << 5,       1,           -1 },
+	{ "feh",              NULL,       NULL,       1 << 5,       1,           -1 },
+	{ "Lxappearance",     NULL,       NULL,       1 << 5,       1,           -1 },
+	{ "Pavucontrol",      NULL,       NULL,       1 << 5,       1,           -1 },
+	{ "Pcmanfm",          NULL,       NULL,       1 << 5,       1,           -1 },
 };
 
 /* layout(s) */
